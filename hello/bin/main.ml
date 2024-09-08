072e64b6691105ce43d79c73c0a1cf91997b7237
brianmcmanus <brianmcmanus2000@gmail.com>
2024-09-08 18:12:04 -0500

    syncing

--- hello/bin/main.ml
+++ hello/bin/main.ml
@@ -1,3 +1,7 @@
 let x = 12;;
 let plus_x y = y + x;;
 let () = print_newline (print_int (plus_x 3));;
+
+(*opam exex -- dune init proj hello*)
+(*opam exec -- dune build*)
+(*opam exec -- dune exec hello*)
