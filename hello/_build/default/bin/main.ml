d859f84a28c1ca9f93d6cb003137c5eb6311cc68
brianmcmanus <brianmcmanus2000@gmail.com>
2024-09-19 11:52:38 -0500

    t

--- hello/_build/default/bin/main.ml
+++ hello/_build/default/bin/main.ml
@@ -1,3 +1,35 @@
-let e = 2.71828
-let divide_e_by x = x /. e
-let () = print_newline (print_float (divide_e_by 1.5));;
+
+
+let p n = print_int(n);print_endline"";n
+
+let d =
+  let f = fun _ ->
+    let rec g c =
+      ( print_int(100); print_endline"";
+        if c > 10 then g (c-1)
+        else ( fun b -> ( p c ) + ( p b ) ) )
+    in g ( p 3 ) ( g ( p 11 )  ( p 6) )
+  in f 50;;
+
+print_int(d);;
+print_endline"";;
+print_endline"";;
+
+
+let p (s, (n:int)) = print_endline(s);n
+let f n =
+  (print_endline("a");
+   fun m ->
+     if (p ("b", n) >= p ("c", 4)) ||
+       ((p ("d", n * 3) > 7) &&
+           (let k = (p ("e", n) - (p("f",3)))
+            in (p ("g", k) <= 5)))
+     then p ("h", (n * m))
+     else (p ("i", (n + m))))
+
+let d = f 2 3;;
+d;;
+
+(*opam exex -- dune init proj hello*)
+(*opam exec -- dune build*)
+(*opam exec -- dune exec hello*)
