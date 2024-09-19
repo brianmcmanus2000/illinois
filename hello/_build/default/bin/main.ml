d79f7739898cc3fa0c99c5d551dbc6f6a68b5483
brianmcmanus <brianmcmanus2000@gmail.com>
2024-09-19 13:46:18 -0500

    more testing for ocaml

--- hello/_build/default/bin/main.ml
+++ hello/_build/default/bin/main.ml
@@ -1,10 +1,12 @@
-
+(*opam exec -- dune init proj hello*)
+(*opam exec -- dune build*)
+(*opam exec -- dune exec hello*)
 
 let p n = print_int(n);print_endline"";n
 
 let d =
   let f = fun _ ->
-    let rec g c =
+    let rec g c = 
       ( print_int(100); print_endline"";
         if c > 10 then g (c-1)
         else ( fun b -> ( p c ) + ( p b ) ) )
@@ -16,20 +18,12 @@ print_endline"";;
 print_endline"";;
 
 
-let p (s, (n:int)) = print_endline(s);n
+let p (s, (n:int)) = print_string(s^" ");print_int n;print_endline"";n
 let f n =
-  (print_endline("a");
+  (print_string("a ");print_int n;print_endline"";
    fun m ->
-     if (p ("b", n) >= p ("c", 4)) ||
-       ((p ("d", n * 3) > 7) &&
-           (let k = (p ("e", n) - (p("f",3)))
-            in (p ("g", k) <= 5)))
+     if (p ("b", n) >= p ("c", 4)) ||  ((p ("d", n * 3) > 7) && (let k = (p ("e", n) - (p("f",3))) in (p ("g", k) <= 5)))
      then p ("h", (n * m))
-     else (p ("i", (n + m))))
+     else (p ("i", (n + m))));;
 
-let d = f 2 3;;
-d;;
-
-(*opam exex -- dune init proj hello*)
-(*opam exec -- dune build*)
-(*opam exec -- dune exec hello*)
+f 2 3
