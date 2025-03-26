3b9456c574ac10558c1c3dcef3991a79705c45d5
brianmcmanus <brianmcmanus2000@gmail.com>
2025-03-26 12:02:13 -0500

    synchronizing

--- networks/quiz8.py
+++ networks/quiz8.py
@@ -2,13 +2,20 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 G = nx.Graph()
-G.add_nodes_from([1,2,3,4,5,6])
-G.add_edges_from([(1,2),(1,3),(1,5)])
-G.add_edges_from([(2,1),(2,3),(2,4)])
-G.add_edges_from([(3,1),(3,2),(3,6)])
-G.add_edges_from([(4,2),(4,5),(4,6)])
-G.add_edges_from([(5,1),(5,4),(5,6)])
-G.add_edges_from([(6,3),(6,4),(6,5)])
-print(nx.average_clustering(G))
+edge_list = [
+   ('e','c'),     
+   ('c','f'), 
+   ('f','i'), 
+   ('i','e'), 
+   ('i','k'), 
+   ('k','j'), 
+   ('f','g'), 
+   ('g','d'), 
+   ('d','h'), 
+   ('h','j'), 
+   ('j','g')    
+]
+G.add_edges_from(edge_list)
+print(nx.clustering(G,['g','d','h','j']))
 nx.draw(G)
 plt.show()
